645a63e800b0780a2ccf01dbc70f2b2a567ccfa6
Shivam mishra <sh6008388@gmail.com>
2023-12-04 23:22:59 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,11 @@
+let github=document.getElementById("github");
+github.addEventListener("click",()=>{
+    window.open("https://github.com/Mishra73","Gitlink")
+})
+let linkedin=document.getElementById("linkedin");
+linkedin.addEventListener("click",()=>{
+    window.open("https://www.linkedin.com/in/satyam-mishra-34b2171b8?trk=contact-info","Gitlink")
+})
+let text=document.getElementById("text");
+let t="I build things for the web & turning ideas into real life products is my calling.";
+
